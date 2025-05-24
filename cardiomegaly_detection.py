932ad5d665ac857be64aeef44b53972557b7f121
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2025-05-24 18:38:07 +0800

    Working on cardiomegaly detection algorithm

--- cardiomegaly_detection.py
+++ cardiomegaly_detection.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+
 import numpy as np
 import skimage
 import torch
@@ -5,15 +8,89 @@ import torchvision
 import matplotlib.pyplot as plt
 import torchxrayvision as xrv
 
-model = xrv.baseline_models.chestx_det.PSPNet()
-img = skimage.io.imread("test_img.png")
-img = xrv.datasets.normalize(img, 255) # convert 8-bit image to [-1024, 1024] range
-img = img[None, ...] # Make single color channel
-transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(),xrv.datasets.XRayResizer(512)])
-img = transform(img)
-img = torch.from_numpy(img)
+def image_to_torch(img: np.ndarray):
+    img = xrv.datasets.normalize(img, 255) # convert 8-bit image to [-1024, 1024] range
+    img = img[None, ...] # Make single color channel
+    transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(),xrv.datasets.XRayResizer(512)])
+    img = transform(img)
+    img = torch.from_numpy(img)
+    return img
 
-with torch.no_grad():
-    pred = model(img)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path")    
+    args = parser.parse_args()
+    
+    if os.path.isdir(args.path):
+        files = os.listdir(args.path)
+        file_paths = [os.path.join(args.path, file) for file in files if file.endswith(".png")]
+        imgs = [skimage.io.imread(file_path) for file_path in file_paths]
+    else:
+        file_paths = [os.path.split(args.path)[1]]
+        imgs = [skimage.io.imread(args.path)]
+        
+    print(len(imgs))
+    
+    model = xrv.baseline_models.chestx_det.PSPNet()
+    for img, filename in zip(imgs, file_paths):
+        img = image_to_torch(img)
+        with torch.no_grad():
+            pred = model(img)
+            
+        targets = list(model.targets)
+        heart_index = targets.index("Heart")
+        l_lung_index = targets.index("Left Lung")
+        r_lung_index = targets.index("Right Lung")
+        index_list = [heart_index, l_lung_index, r_lung_index]
+        
+        print(pred[0, heart_index])
+        
+        pred = 1 / (1 + np.exp(-pred))  # sigmoid
+        pred[pred < 0.5] = 0
+        pred[pred > 0.5] = 1
+        
+        print(pred[0, heart_index])
 
+        heart_width = 0
+        
+        for col in range(pred[0, heart_index].shape[1]-1, -1, -1):
+            if pred[0, heart_index][col].sum() > 0:
+                print(col)
+                heart_width = col
+                break
 
+        for col in range(pred[0, heart_index].shape[1]):
+            if pred[0, heart_index][col].sum() > 0:
+                print(col)
+                heart_width -= col
+                break
+
+        lungs_width = 0
+        
+        #r lung rightmost
+        for col in range(pred[0, r_lung_index].shape[1]-1, -1, -1):
+            if pred[0, r_lung_index][col].sum() > 0:
+                print(col)
+                lungs_width = col
+                break
+
+        #l lung leftmost 
+        for col in range(pred[0, l_lung_index].shape[1]):
+            if pred[0, r_lung_index][col].sum() > 0:
+                print(col)
+                lungs_width -= col
+                break
+        
+        cardiomegaly = heart_width >= 0.5 * lungs_width
+        
+        print(f"""File name: {filename}
+        Lungs width: {lungs_width}
+        Heart width: {heart_width}
+        Cardiomegaly: {cardiomegaly}""")
+    
+    """fig, axs = plt.subplots(2,2)
+    axs[0,0].imshow(img[0])
+    axs[0,1].imshow(pred[0,heart_index])
+    axs[1,0].imshow(pred[0,l_lung_index])
+    axs[1,1].imshow(pred[0,r_lung_index])
+    plt.show()"""
